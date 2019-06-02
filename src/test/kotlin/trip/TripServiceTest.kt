183bfbecdb4335ce8385e048b56b2c13cdca73bd
David Gomez <david.gomez@eydevelopment.com>
2019-06-02 12:33:23 +0200

    Add second test, Refactor

--- src/test/kotlin/trip/TripServiceTest.kt
+++ src/test/kotlin/trip/TripServiceTest.kt
@@ -5,7 +5,14 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.MockitoAnnotations
+import org.mockito.internal.matchers.Null
 import org.tripservicekata.exception.UserNotLoggedInException
+import org.tripservicekata.trip.TripServiceTest.Constants.Companion.ANOTHER_USER
+import org.tripservicekata.trip.TripServiceTest.Constants.Companion.GUEST
+import org.tripservicekata.trip.TripServiceTest.Constants.Companion.REGISTERED_USER
+import org.tripservicekata.trip.TripServiceTest.Constants.Companion.UNUSED_USER
+
+private var loggedInUser: User? = null
 
 class TripServiceTest {
 
@@ -18,14 +25,38 @@ class TripServiceTest {
     @Test
     fun should_throw_user_not_logged_exception_when_user_is_not_logged_in() {
         val tripService = TestableTripService()
+        loggedInUser = GUEST
         Assertions.assertThrows(UserNotLoggedInException::class.java) {
             tripService.getTripsByUser(User())
         }
     }
 
+    @Test
+    fun should_not_return_trips_when_users_are_not_friends() {
+        val tripService = TestableTripService()
+
+        loggedInUser = REGISTERED_USER
+
+        val friend = User()
+        friend.addFriend(ANOTHER_USER)
+
+        val friendTrips = tripService.getTripsByUser(friend)
+
+        Assertions.assertEquals(friendTrips.size, 0)
+    }
+
     private class TestableTripService : TripService() {
         override fun getLoggetInUser(): User? {
-            return null
+            return loggedInUser
+        }
+    }
+
+    class Constants {
+        companion object {
+            val GUEST = null
+            val UNUSED_USER = null
+            val REGISTERED_USER = User()
+            val ANOTHER_USER = User()
         }
     }
 }
