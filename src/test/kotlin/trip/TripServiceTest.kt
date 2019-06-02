60064c0cce52315a9236abba64f8d3c6b73a9334
David Gomez <david.gomez@eydevelopment.com>
2019-06-02 12:05:20 +0200

    Add first test

--- src/test/kotlin/trip/TripServiceTest.kt
+++ src/test/kotlin/trip/TripServiceTest.kt
@@ -1,5 +1,25 @@
 package org.tripservicekata.trip
 
+import org.craftedsw.tripservicekata.user.User
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.MockitoAnnotations
+import org.tripservicekata.exception.UserNotLoggedInException
+
 class TripServiceTest {
 
+    @BeforeEach
+    internal fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+    }
+
+    @Test
+    fun should_throw_user_not_logged_exception_when_user_is_not_logged_in() {
+        val tripService = TripService()
+        Assertions.assertThrows(UserNotLoggedInException::class.java) {
+            tripService.getTripsByUser(User())
+        }
+    }
 }
